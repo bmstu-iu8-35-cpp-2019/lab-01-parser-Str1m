28d324dd953a6231b36e2d1cf8cad631c5d48246
Str1m <q.st.stream@gmail.com>
2019-09-10 18:02:19 +0300

    Fixes main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,21 +3,21 @@
 using json = nlohmann::json;
 int main() {
     json j;
-    j["ticker"]={"Si-9.15", "RTS-9.15", "GAZP-9.15"};
-    j["id"]={100024, 100027, 100050};
-    j["description"]={"Futures contract for USD/RUB",
+    j["ticker"] = {"Si-9.15", "RTS-9.15", "GAZP-9.15"};
+    j["id"] = {100024, 100027, 100050};
+    j["description"] = {"Futures contract for USD/RUB",
                       "Futures contract for index RTS",
                       "Futures contract for Gazprom shares"};
-    json j1=json::array();
+    json j1 = json::array();
     std::vector <json> v1;
     json obj;
     v1.resize(j["ticker"].size());
-    for (size_t i=0;i<j["ticker"].size();i++){
-        v1[i]["id"]=j["id"][i];
-        v1[i]["ticker"]=j["ticker"][i];
-        v1[i]["description"]=j["description"][i];
+    for (size_t i = 0; i < j["ticker"].size(); i++){
+        v1[i]["id"] = j["id"][i];
+        v1[i]["ticker"] = j["ticker"][i];
+        v1[i]["description"] = j["description"][i];
     }
-    for (size_t i=0;i<3;i++){
+    for (size_t i = 0; i < 3; i++){
         j1.push_back(v1[i]);
     }
     std::cout << j1;

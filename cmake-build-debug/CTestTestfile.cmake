daa91f2302b621e5d079c318e35e2197876f0556
Str1m <q.st.stream@gmail.com>
2020-01-17 18:54:46 +0300

    Delete cmake-build-debug

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/egor/Univercity/Sem_3/git/lab-01-parser-Str1m
-# Build directory: /Users/egor/Univercity/Sem_3/git/lab-01-parser-Str1m/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/Users/egor/Univercity/Sem_3/git/lab-01-parser-Str1m/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/egor/Univercity/Sem_3/git/lab-01-parser-Str1m/CMakeLists.txt;63;add_test;/Users/egor/Univercity/Sem_3/git/lab-01-parser-Str1m/CMakeLists.txt;0;")
